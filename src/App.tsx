21e7e8af003c411f2447811a6596c1586c2f2281
akshit <akshits987@gmail.com>
2024-08-12 15:49:48 +0530

    build updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
-import { RegisterModal } from "./components/Register";
 
 const App = () => {
   return (
